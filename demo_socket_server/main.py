f4c7babe023eedca595d59ed6854693984d2a186
fba <fbaltia@gmail.com>
2026-08-03 16:53:27 +0200

    Add socket server functionality and client integration for real-time messaging

--- demo_socket_server/main.py
+++ demo_socket_server/main.py
@@ -1,13 +1,39 @@
-import uvicorn
 import socketio
+import uvicorn
+import datetime
 
 sio = socketio.AsyncServer(async_mode='asgi')
 app = socketio.ASGIApp(sio)
 
-
-if __name__ == '__main':
-    uvicorn.run('main:app', '127.0.0.1', port=8000, reload = True)
-
-    
+clients = {}
+tokens = {}
+token_def = ["⬤","✖"]
 
 
+
+@sio.event
+async def connect(sid, environ, auth):
+    tokens[sid] = token_def[0] if not clients else token_def[1]
+    clients[sid] = auth.get('username')
+    print(f'{datetime.datetime.now()} - {auth.get('username')} s\'est connecté, jeton : {tokens[sid]}')
+    await sio.emit('ON_CONNECT', {
+            'username': clients.get(sid),
+            'token': tokens.get(sid),
+            'tokens': token_def
+        })
+
+@sio.event
+def disconnect(sid):
+    print(f'{clients.get(sid)} s\'est déco')
+    clients.pop(sid)
+
+@sio.on('NEW_MESSAGE')
+async def new_message(sid, message: str):
+    print(clients[sid], message)
+    await sio.emit('ON_MESSAGE', {
+        'username': clients.get(sid),
+        'message': message
+    })
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
